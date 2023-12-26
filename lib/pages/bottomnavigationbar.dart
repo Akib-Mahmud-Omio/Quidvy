3d75b9d3bf22fbb414ab6d60f0977bad4df684bd
Reza066 <reza.cse.20220104066@aust.edu>
2023-12-27 01:29:46 +0600

    added navigation icons

--- lib/pages/bottomnavigationbar.dart
+++ lib/pages/bottomnavigationbar.dart
@@ -1,2 +1,102 @@
-class Bottomnavigationbar {
+import 'package:flutter/material.dart';
+
+
+
+class Bottom extends StatefulWidget {
+  const Bottom({Key? key}) : super(key: key);
+
+  @override
+  State<Bottom> createState() => _BottomState();
+}
+
+class _BottomState extends State<Bottom> {
+  int index = 2;
+  List Screen = [app_drawer(), Statistics(), Home(), Statistics()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Screen[index],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add,
+          size: 30,
+        ),
+        backgroundColor: Color(0xfff25a18),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BottomAppBar(
+        height: 60,
+        shape: CircularNotchedRectangle(),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 4, bottom: 4),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index = 0;
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 25.0),
+                  child: Icon(
+                    Icons.menu,
+                    size: 30,
+                    color: index == 0 ?  Color(0xfff25a18)  : Colors.blueGrey,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index = 1;
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0), // Added padding
+                  child: Icon(
+                    Icons.pie_chart_rounded,
+                    size: 30,
+                    color: index == 1 ? Color(0xfff25a18) : Colors.blueGrey,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index = 2;
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 23.0), // Added padding
+                  child: Icon(
+                    Icons.home,
+                    size: 30,
+                    color: index == 2 ?  Color(0xfff25a18) : Colors.blueGrey,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index = 3;
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 28.0), // Added padding
+                  child: Icon(
+                    Icons.account_balance_wallet_outlined,
+                    size: 30,
+                    color: index == 3 ? Color(0xfff25a18) : Colors.blueGrey,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
